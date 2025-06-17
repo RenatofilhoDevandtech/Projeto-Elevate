07790e83b4cb522b2fca5ad937fd5755697f7604
analessp <lessakesia07@gmail.com>
2025-06-17 12:50:43 -0300

    criando content creation page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ProfileTestPage from './pages/ProfileTestPage';
 import InterviewSimPage from './pages/InterviewSimPage';
 import CommunityPage from './pages/CommunityPage';
 import CertificatesPage from './pages/CertificatesPage';
+import ContentCreationPage from './pages/ContentCreationPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path="/simulador-entrevista" element={<InterviewSimPage />} />
         <Route path="/comunidade" element={<CommunityPage />} />
         <Route path="/certificados" element={<CertificatesPage />} />
+        <Route path="/publicar-conteudo" element={<ContentCreationPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
