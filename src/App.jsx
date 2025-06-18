b52a827d5915668fe2a3142a6c592d38fb691d34
RenatoFilho <https://github.com/RenatofilhoDevandtech>
2025-06-18 14:10:44 -0300

    Ajustes na estrutura do App e melhorias na organização

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// Layouts e Componentes de Rota
 import MainLayout from './layouts/MainLayout';
+import ProtectedRoute from './components/Common/ProtectedRoute';
+
+// Páginas
 import HomePage from './pages/HomePage';
 import LearningPathsPage from './pages/LearningPathsPage';
 import PathDetailsPage from './pages/PathDetailsPage';
 import ProfileTestPage from './pages/ProfileTestPage';
+import TestResultPage from './pages/TestResultPage';
 import InterviewSimPage from './pages/InterviewSimPage';
-import CommunityPage from './pages/CommunityPage';
+import DashboardPage from './pages/DashboardPage';
 import CertificatesPage from './pages/CertificatesPage';
 import ContentCreationPage from './pages/ContentCreationPage';
+import CommunityPage from './pages/CommunityPage';
+import PublicCertificatePage from './pages/PublicCertificatePage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/trilhas" element={<LearningPathsPage />} />
-        <Route path="/trilha/:pathId" element={<PathDetailsPage />} />
-        <Route path="/teste-perfil" element={<ProfileTestPage />} />
-        <Route path="/simulador-entrevista" element={<InterviewSimPage />} />
-        <Route path="/comunidade" element={<CommunityPage />} />
-        <Route path="/certificados" element={<CertificatesPage />} />
-        <Route path="/publicar-conteudo" element={<ContentCreationPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+      {/* Rotas Públicas */}
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="trilhas" element={<LearningPathsPage />} />
+        <Route path="trilha/:pathId" element={<PathDetailsPage />} />
+        <Route path="comunidade" element={<CommunityPage />} />
+        <Route path="certificados" element={<CertificatesPage />} />
+        <Route path="publicar-conteudo" element={<ContentCreationPage />} />
       </Route>
+
+      {/* Rota pública especial sem o layout principal */}
+      <Route path="/certificado/:uniqueCode" element={<PublicCertificatePage />} />
+
+      {/* Rotas Privadas (protegidas pelo componente ProtectedRoute) */}
+      <Route path="/perfil" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+      <Route path="/teste-perfil" element={<ProtectedRoute><ProfileTestPage /></ProtectedRoute>} />
+      <Route path="/teste/resultado" element={<ProtectedRoute><TestResultPage /></ProtectedRoute>} />
+      <Route path="/simulador" element={<ProtectedRoute><InterviewSimPage /></ProtectedRoute>} />
+
+      {/* Rota 404 */}
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
